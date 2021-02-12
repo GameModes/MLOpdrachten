79c522913759e040e8e5f793531d2c145a822e72
ruben <rubenski0345@gmail.com>
2021-02-12 15:27:36 +0100

    Documentation Added

--- P1/Perceptron.py
+++ P1/Perceptron.py
@@ -3,47 +3,43 @@ class Perceptron():
         self.bias = bias
         self.weights = weights
 
-    def give_correct_possibilities(self):
-        possible_inputs1 = [(0), (1)]
-        possible_inputs2 = [(0, 0), (1, 0), (0, 1), (1, 1)]
-        possible_inputs3 = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (1, 1, 0), (0, 0, 1), (1, 0, 1), (0, 1, 1), (1, 1, 1)]
-        if len(self.weights)  == 1:
-            allposibleinputs = possible_inputs1
-        elif len(self.weights) == 2:
-            allposibleinputs = possible_inputs2
-        else:
-            allposibleinputs = possible_inputs3
-        return allposibleinputs
-
     def activation(self):
-        allposibleinputs = self.give_correct_possibilities()
+        """
+        Vraagt alle mogelijkheden voor inputs in een list
+        Zet alle mogelijkheden in een dictionary met als value een lege list die ingevuld kan worden
+        loop door inputs en tel bij elke mogelijke input de totale gewicht van de inputs
+        Dan wordt gecheckt of het met de bias positief is, zo ja dan is het een 1 als output en wordt het
+        in de lege list van de dictionary toegevoegd
+        :return:
+        """
+        from itertools import product
+        allposibleinputs = [i for i in product(range(2), repeat=len(self.weights))]
         # Voeg de activatiefunctie toe om uitvoer (output) te leveren bij een gegeven invoer (input).
-        possibilityOutputList = {i : None for i in allposibleinputs} #put all posibilities in a dictionary to save the output
+        possibilityOutputList = {i : [] for i in allposibleinputs} #put all posibilities in a dictionary to save the output
         for possibility in range(len(allposibleinputs)):
             totalsum = 0
-            # print(possibility)
-            # print(allposibleinputs)
-            try:
-                for index in range(len(allposibleinputs[possibility])): #dit is mijn manier om de perceptron uit te rekenen
-                    totalsum += allposibleinputs[possibility][index] * self.weights[index] #waarbij de totaal antwoord elk antwoord toegevoegd wordt
-            except TypeError:
-                totalsum += possibility * self.weights[0]
+            for index in range(len(allposibleinputs[possibility])):
+                totalsum += allposibleinputs[possibility][index] * self.weights[index] #waarbij de totaal antwoord elk antwoord toegevoegd wordt
             output = totalsum - self.bias #de som in een variabele gedaan
             if output >= 0:#en te checken of dit positief is
-                possibilityOutputList[allposibleinputs[possibility]] = 1
-                # possibilityOutputList.append(1)
+                possibilityOutputList[allposibleinputs[possibility]].append(1)
             else:
-                possibilityOutputList[allposibleinputs[possibility]] =0
-                # possibilityOutputList.append(0)
+                possibilityOutputList[allposibleinputs[possibility]].append(0)
         return possibilityOutputList
 
     def gateUnderstander(self, usedGate, amountofinputs):
+        """
+        wordt aangeroepen om een String GateNaam om te zetten in een Perceptron met de getelde aantal inputs als gewichten
+        :param usedGate:
+        :param amountofinputs:
+        :return:
+        """
         savedGates = {
             "INVERT": Perceptron(weights=[-1], bias=0),
             "AND": Perceptron(weights=[1 for inputamount in range(amountofinputs)], bias=amountofinputs*1),
             "OR": Perceptron(weights=[0.5 for inputamount in range(amountofinputs)], bias=0.5),
             "NOR": Perceptron(weights=[-1 for inputamount in range(amountofinputs)], bias=0),
-            "NAND": Perceptron(weights=[-1 for inputamount in range(amountofinputs)], bias=amountofinputs*-1.5)}
+            "NAND": Perceptron(weights=[-1.5 for inputamount in range(amountofinputs)], bias=amountofinputs*-1)}
         return savedGates[usedGate]
 
     def get_weights(self):
@@ -63,13 +59,13 @@ class Perceptron():
         return self.weights, self.bias
 
 
-# print('\nAND Port')
-# AND_port = Perceptron(weights=[0.5, 0.5], bias=1)
-# print(AND_port.activation())
+print('\nAND Port')
+AND_port = Perceptron(weights=[0.5, 0.5], bias=1)
+print(AND_port.activation())
 #
-# print('\nInvert Port')
-# INVERT_port = Perceptron(weights=[-1], bias=0)
-# print(INVERT_port.activation())
+print('\nInvert Port')
+INVERT_port = Perceptron(weights=[-1], bias=0)
+print(INVERT_port.activation())
 #
 # #
 # print('\nAND Port')
--- P1/PerceptronLayer.py
+++ P1/PerceptronLayer.py
@@ -3,6 +3,15 @@ import Perceptron as per
 class PerceptronLayer:
     layer = None
     def __init__(self, gatesInLayer, listOfAmountOfInputs): #vraagt op de gatesindelayer en welk naam ze hebben (misschien moet ik de layer nummer er bij zetten)
+        """
+        zet de default perceptron op lege weights en bias
+        loopt door alle gates van de inputs
+        checkt of de gates goed zijn ingevoerd
+        voert elke gate in de gateunderstander van perceptron.py om de perceptron op te vragen en het in een layer toe te voegen
+        de "for x in range(len(layer))" zorgt voor de uitkomsten, bij het gebruik van meerdere gates in een layer, samen in de value
+        dictionary komen om het overzichtelijker te maken voor gebruikers en makkelijker waardes eruit te halen voor de volgende laag
+        :rtype: object
+        """
         listsOfGatesinLayer = []
         layer = []
         p = per.Perceptron(weights=[], bias=0) #default
@@ -13,10 +22,14 @@ class PerceptronLayer:
                 print("Gate or Entire Input wrongly typed")
                 break
         for x in range(len(layer)):
-            try:
+            if x == 0:
                 listsOfGatesinLayer.append(layer[x].activation())
-            except IndexError:
-                listsOfGatesinLayer.append(layer[x].activation())
+            else:
+                Outputlist = layer[x].activation()
+                print(Outputlist )
+                for key, value in Outputlist.items():
+                    listsOfGatesinLayer[0][key].append(value[0])
+
 
         self.listsOfGatesinLayer = listsOfGatesinLayer
         self.layer = layer
@@ -24,8 +37,10 @@ class PerceptronLayer:
     def __str__(self):
         return str(self.listsOfGatesinLayer) #waarschijnlijk wilde ik hier ene list terug geven van alle gates, maar dat is niet goed gegaan
 
-pl = PerceptronLayer(["NAND", "OR", "INVERT"], [2, 2, 1])
-print(pl)
+# pl = PerceptronLayer(["NAND", "OR", "INVERT"], [2, 2, 1])
+# print(pl)
 
+pl = PerceptronLayer(["OR", "NAND"], [2, 2])
+print(pl)
 
 # print(pl.layer[0].activation([1,1]))
